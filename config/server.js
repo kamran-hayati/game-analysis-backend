bdd29180bb8674e240759c270b6bb28103c40d97
Kamran H <kamran.tmu.math>
2024-01-19 09:35:13 -0500

    Update config/[server|admin].js clarifying admin path

--- config/server.js
+++ config/server.js
@@ -1,10 +1,16 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  admin: {
+    url: env('STRAPI_ADMIN_URL'),
+  },
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+  dirs: {
+    public: "",
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
